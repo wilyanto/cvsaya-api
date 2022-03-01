898c04091232fe759ea4569a20e709d22b6ff338
Albert <albertbobby94@gmail.com>
2022-03-01 11:06:57 +0700

    Add change expected position foreign to new table

---
+++ database/migrations/2022_03_01_030424_candidate_positions.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('candidate_positions', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->boolean('is_validate')->default(false);
+            $table->bigInteger('inserted_by')->unsigned();
+            $table->timestamps();
+        });
+
+        Schema::table('cv_expected_positions',function(Blueprint $table){
+            $table->foreign('expected_position')->references('id')->on('candidate_positions');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('cv_expected_positions', function (Blueprint $table) {
+            $table->dropForeign(['expected_position']);
+        });
+
+
+        Schema::dropIfExists('candidate_positions');
+    }
+};
