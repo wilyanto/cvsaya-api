ef4f8e6b8c418120f54459228ef55f88ef4395b3
Albert <albertbobby94@gmail.com>
2022-03-19 14:39:34 +0700

    add table master database

---
+++ database/migrations/2022_03_19_063629_add_degree_as_object.php
@@ -0,0 +1,58 @@
+<?php
+
+use App\Models\CvExperience;
+use App\Models\Degree;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('degree', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::table('cv_educations', function (Blueprint $table) {
+            $table->bigInteger('degree_id')->unsigned()->nullable();
+        });
+
+        Schema::table('cv_educations', function (Blueprint $table) {
+            $table->foreign('degree_id')->references('id')->on('degree');
+        });
+
+        Schema::table('cv_educations', function (Blueprint $table) {
+            $table->dropColumn('degree');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+        Schema::table('cv_educations', function (Blueprint $table) {
+            $table->string('degree');
+        });
+
+        Schema::table('cv_educations', function (Blueprint $table) {
+            $table->dropForeign(['degree_id']);
+        });
+        Schema::table('cv_educations', function (Blueprint $table) {
+            $table->dropColumn('degree_id');
+        });
+
+        Schema::drop('degree');
+    }
+};
