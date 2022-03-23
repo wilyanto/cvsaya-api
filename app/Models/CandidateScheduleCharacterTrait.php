41690291fa1fea904231a2ef273f1c6b1e905e53
Albert <albertbobby94@gmail.com>
2022-03-23 13:27:52 +0700

    add Fillable on ScheduleCharacterTrait

--- app/Models/CandidateScheduleCharacterTrait.php
+++ app/Models/CandidateScheduleCharacterTrait.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class CandidateScheduleCharacterTrait extends Model
 {
     use HasFactory;
+
+    public $fillable = [
+        'candidate_employee_schedule_id',
+        'character_trait_id'
+    ];
 }
