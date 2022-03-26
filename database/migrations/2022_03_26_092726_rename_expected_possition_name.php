17240bbb19214f451d7ca3e7d6e755dce671bf87
Albert <albertbobby94@gmail.com>
2022-03-26 16:56:54 +0700

    add rename column expected_position

--- database/migrations/2022_03_26_092726_rename_expected_possition_name.php
+++ database/migrations/2022_03_26_092726_rename_expected_possition_name.php
@@ -18,6 +18,10 @@ return new class extends Migration
             $table->renameColumn('expected_salary_id', 'expected_jobs_id');
         });
 
+        Schema::table('cv_expected_positions', function (Blueprint $table) {
+            $table->dropForeign(['expected_position']);
+        });
+
         Schema::rename('cv_expected_positions', 'cv_expected_jobs');
 
         Schema::rename('cv_log_expected_positions', 'cv_log_expected_jobs');
@@ -25,6 +29,11 @@ return new class extends Migration
         Schema::table('cv_log_expected_jobs', function (Blueprint $table) {
             $table->foreign('expected_jobs_id')->references('id')->on('cv_expected_jobs');
         });
+
+        Schema::table('cv_expected_jobs',function(Blueprint $table){
+            $table->foreign('expected_position')->references('id')->on('candidate_positions');
+        });
+
     }
 
     /**
@@ -39,12 +48,20 @@ return new class extends Migration
             $table->renameColumn('expected_jobs_id', 'expected_salary_id');
         });
 
+        Schema::table('cv_expected_jobs', function (Blueprint $table) {
+            $table->dropForeign(['expected_position']);
+        });
+
         Schema::rename('cv_expected_jobs', 'cv_expected_positions');
 
         Schema::rename('cv_log_expected_jobs','cv_log_expected_positions');
 
         Schema::table('cv_log_expected_positions', function (Blueprint $table) {
             $table->foreign('expected_salary_id')->references('id')->on('cv_expected_positions');
+        });
+
+        Schema::table('cv_expected_positions',function(Blueprint $table){
+            $table->foreign('expected_position')->references('id')->on('candidate_positions');
         });
     }
 };
