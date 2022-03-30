b83eaff3277cd31410e9e945a8b95a577758e68a
Albert <albertbobby94@gmail.com>
2022-03-30 16:37:34 +0700

    WIP, rename foreign

--- database/migrations/2022_03_30_073935_rename_employee.php
+++ database/migrations/2022_03_30_073935_rename_employee.php
@@ -13,7 +13,15 @@ return new class extends Migration
      */
     public function up()
     {
+        Schema::table('candidate_employees', function (Blueprint $table) {
+            $table->dropForeign('suggest_by');
+        });
         Schema::rename('candidate_employees', 'candidate');
+
+        Schema::table('candidate', function (Blueprint $table) {
+            $table->foreign('suggest_by')->references('id')->on('employee_details');
+        });
+
         Schema::rename('candidate_employee_schedules', 'candidate_interview_schedules');
         Schema::rename('candidate_employee_schedule_character_traits', 'candidate_interview_schedules_character_traits');
 
@@ -26,7 +34,15 @@ return new class extends Migration
      */
     public function down()
     {
+        Schema::table('candidate', function (Blueprint $table) {
+            $table->dropForeign('suggest_by');
+        });
         Schema::rename('candidate','candidate_employees');
+
+        Schema::table('candidate_employees', function (Blueprint $table) {
+            $table->foreign('suggest_by')->references('id')->on('employee_details');
+        });
+
         Schema::rename('candidate_interview_schedules','candidate_employee_schedules');
         Schema::rename('candidate_interview_schedules_character_traits','candidate_employee_schedule_character_traits');
     }
