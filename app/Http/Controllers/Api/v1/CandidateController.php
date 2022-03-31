4ee28803f92fd6e5233911de77ddac1ee03afad9
Albert <albertbobby94@gmail.com>
2022-03-31 08:07:23 +0700

    change interview_by to interview_at

--- app/Http/Controllers/Api/v1/CandidateController.php
+++ app/Http/Controllers/Api/v1/CandidateController.php
@@ -267,7 +267,7 @@ class CandidateController extends Controller
             }
 
             $data = $request->all();
-            $data['interview_by'] = date('Y-m-d H:i:s',strtotime($data['interview_by']));
+            $data['interview_at'] = date('Y-m-d H:i:s',strtotime($data['interview_by']));
             $data['candidate_id'] = $id;
 
             $candidateEmpolyeeSchedule = CandidateInterviewSchedule::create($data);
