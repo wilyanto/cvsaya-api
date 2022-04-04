4006ce1047b95fa5c186fb3b0f250ce175e29f36
Albert <albertbobby94@gmail.com>
2022-04-04 17:20:34 +0700

    change back return to object

--- app/Http/Controllers/Api/v1/CvDocumentController.php
+++ app/Http/Controllers/Api/v1/CvDocumentController.php
@@ -352,7 +352,7 @@ class CvDocumentController extends Controller
             'original_file_name' => $request->file->getClientOriginalName(),
         ]);
 
-        return $this->showOne((string)$document->id);
+        return $this->showOne($document);
     }
 
     /**
