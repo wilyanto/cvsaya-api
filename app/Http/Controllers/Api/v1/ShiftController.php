a341c267f0293e13ada3df7c2bc7e8a44743ca93
Wilyanto <wilyanto24@gmail.com>
2022-05-13 14:44:35 +0700

    added me/shift filtered on certain date

--- app/Http/Controllers/Api/v1/ShiftController.php
+++ app/Http/Controllers/Api/v1/ShiftController.php
@@ -161,17 +161,18 @@ class ShiftController extends Controller
         return $this->showOne('Success');
     }
 
-    public function getShift()
+    public function getShift(Request $request)
     {
+        $date = $request->date;
         $employee = Employee::where('user_id', auth()->id())->firstOrFail();
 
-        $employeeOneTimeShifts = $employee->getTodayOneTimeShifts();
+        $employeeOneTimeShifts = $employee->getCertainDateOneTimeShifts($date);
 
         if ($employeeOneTimeShifts->isNotEmpty()) {
             return $this->showAll($employeeOneTimeShifts);
         }
 
-        $employeeRecurringShifts = $employee->getTodayRecurringShifts();
+        $employeeRecurringShifts = $employee->getCertainDateRecurringShifts($date);
 
         if ($employeeRecurringShifts->isNotEmpty()) {
             return $this->showAll($employeeRecurringShifts);
