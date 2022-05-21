d0e8f145a4410df6b83e21a3c1f8855b18bfef58
William <Williamliang99@gmail.com>
2022-05-21 12:24:39 +0700

    fix calling candidates on wrong function

--- app/Models/Candidate.php
+++ app/Models/Candidate.php
@@ -140,7 +140,7 @@ class Candidate extends Model implements Auditable
             'education' => $this->educations->first(),
             'gender' =>  $this->profile->gender,
             'position' => $this->job->position,
-            'domicile' => $this->domicile->toArray(),
+            'domicile' => $this->domicile->province(),
         ];
     }
 
@@ -183,7 +183,7 @@ class Candidate extends Model implements Auditable
             'profile' => $this->profile,
             'latest_result' => $this->label(),
             'registered_at' => $this->registered_at,
-            'domicile' => $this->domicile->province(),
+            'domicile' => $this->domicile,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
