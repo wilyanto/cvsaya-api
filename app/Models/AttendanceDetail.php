38c3f2cc9f60e9650f6a5091920d66822a4df01f
William <Williamliang99@gmail.com>
2022-06-13 16:17:51 +0700

    add attendance detail model

---
+++ app/Models/AttendanceDetail.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\UuidGenerator;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use MatanYadaev\EloquentSpatial\SpatialBuilder;
+
+class AttendanceDetail extends Model
+{
+    use HasFactory, UuidGenerator;
+
+    protected $fillable = [
+        'attendance_type',
+        'attended_at',
+        'scheduled_at',
+        'attendance_qr_code_id',
+        'image',
+        'location',
+        'ip',
+        'verified_by',
+        'verified_at'
+    ];
+
+    protected $casts = [
+        'location' => Point::class
+    ];
+
+    public function newEloquentBuilder($query): SpatialBuilder
+    {
+        return new SpatialBuilder($query);
+    }
+
+    public function attendancePenalty()
+    {
+        return $this->hasOne(AttendancePenalty::class, 'attendance_id', 'id');
+    }
+
+    public function outsideRadiusAttendance()
+    {
+        return $this->hasOne(OutsideRadiusAttendance::class, 'attendance_id', 'id');
+    }
+}
