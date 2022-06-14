389c5d12b829495081683b7aef60aef2c2ff4fc6
William <Williamliang99@gmail.com>
2022-06-14 11:34:02 +0700

    align attendance penalty cron

--- app/Console/Commands/DailyAttendancePenalty.php
+++ app/Console/Commands/DailyAttendancePenalty.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use App\Enums\AttendanceType;
 use App\Models\Attendance;
+use App\Models\AttendanceDetail;
 use App\Models\AttendanceEmployee;
 use App\Models\AttendancePenalty;
 use App\Models\Employee;
@@ -13,6 +14,7 @@ use Carbon\Carbon;
 use Hamcrest\Type\IsBoolean;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
+use MatanYadaev\EloquentSpatial\Objects\Point;
 
 class DailyAttendancePenalty extends Command
 {
@@ -37,141 +39,104 @@ class DailyAttendancePenalty extends Command
      */
     public function handle()
     {
-        // TODO: need to discuss either need to create attendance or not
-        // how to handle if attendance more than 1 shift (more than 1 clock_in, clock_out, etc)
-        $employees = Employee::get();
+        $attendances = Attendance::where('date', today())->get();
         $today = today();
-        foreach ($employees as $employee) {
-            Log::info($employees);
-            $company = $employee->company;
-            $companyId = $company->id;
-            $employeeShifts = $employee->getShifts($today);
-            foreach ($employeeShifts as $employeeShift) {
-                // one time shift no penalty?
-                if ($employeeShift instanceof EmployeeOneTimeShift) {
-                    return;
-                }
-                $isBreakInPenalty = true;
-                $todayAttendances = $employeeShift->getAttendances($today);
 
-                // no clock in
-                $clockInAttendance = $todayAttendances->where('attendance_type', AttendanceType::clockIn())
-                    ->where('shift_id', $employeeShift->shift_id)
-                    ->where('employee_id', $employee->id)
-                    ->whereDate('scheduled_at', $today)
-                    ->first();
-                if (!$clockInAttendance) {
-                    return;
-                }
+        foreach ($attendances as $attendance) {
+            $isBreakInPenalty = true;
+            $shift = $attendance?->shift;
+            $company = $attendance?->employee?->company;
 
-                // no clock out
-                $clockOutAttendance = $todayAttendances->where('attendance_type', AttendanceType::clockOut())
-                    ->where('shift_id', $employeeShift->shift_id)
-                    ->where('employee_id', $employee->id)
-                    ->whereDate('scheduled_at', $today)
+            if (!$attendance->clockInAttendanceDetail) {
+                continue;
+            }
+
+            if (!$attendance->clockOutAttendanceDetail) {
+                $shiftTime = new Carbon($shift->clock_out);
+
+                $penalty = Penalty::where('attendance_type', AttendanceType::clockOut())
+                    ->where('company_id', $company->id)
+                    ->whereNull('lateness')
                     ->first();
-                if (!$clockOutAttendance) {
-                    $shiftTime = new Carbon($employeeShift->shift->clock_out);
-                    $penalty = Penalty::where('attendance_type', AttendanceType::clockOut())
-                        ->where('company_id', $companyId)
-                        ->whereNull('lateness')
-                        ->first();
-                    $attendance = Attendance::create([
-                        'attendance_type' => AttendanceType::clockOut(),
-                        'attended_at' => null,
-                        'scheduled_at' => $today->copy()->addSeconds($shiftTime->secondsSinceMidnight()),
-                        'attendance_qr_code_id' => null,
-                        'image' => null,
-                        'ip' => '127.0.0.1',
-                        'longitude' => null,
-                        'latitude' => null,
-                        'verified_by' => null,
-                        'verified_at' => null,
-                        'shift_id' => $employeeShift->shift_id,
-                        'employee_id' => $employee->id
-                    ]);
+                $attendanceDetail = AttendanceDetail::create([
+                    'attendance_type' => AttendanceType::clockOut(),
+                    'attended_at' => null,
+                    'scheduled_at' => $today->copy()->addSeconds($shiftTime->secondsSinceMidnight()),
+                    'attendance_qr_code_id' => null,
+                    'image' => null,
+                    'ip' => '127.0.0.1',
+                    'location' => null,
+                    'verified_by' => null,
+                    'verified_at' => null,
+                ]);
+                AttendancePenalty::create([
+                    'penalty_amount' => $penalty->amount,
+                    'attendance_detail_id' => $attendanceDetail->id,
+                    'penalty_id' => $penalty->id,
+                    'penalty_name' => $penalty->name,
+                    'note' => 'Tidak Scan Pulang'
+                ]);
+                $attendance->update(['clock_out_id' => $attendanceDetail->id]);
+            }
+
+            if (!$attendance->startBreakAttendanceDetail) {
+                $shiftTime = new Carbon($shift->break_started_at);
+
+                $penalty = Penalty::where('attendance_type', AttendanceType::breakTime())
+                    ->where('company_id', $company->id)
+                    ->whereNull('lateness')
+                    ->first();
+                $attendanceDetail = AttendanceDetail::create([
+                    'attendance_type' => AttendanceType::breakStartedAt(),
+                    'attended_at' => null,
+                    'scheduled_at' => $today->copy()->addSeconds($shiftTime->secondsSinceMidnight()),
+                    'attendance_qr_code_id' => null,
+                    'image' => null,
+                    'ip' => '127.0.0.1',
+                    'location' => null,
+                    'verified_by' => null,
+                    'verified_at' => null,
+                ]);
+                AttendancePenalty::create([
+                    'penalty_amount' => $penalty->amount,
+                    'attendance_detail_id' => $attendanceDetail->id,
+                    'penalty_id' => $penalty->id,
+                    'penalty_name' => $penalty->name,
+                    'note' => 'Tidak Scan Istirahat Keluar'
+                ]);
+                $attendance->update(['start_break_id' => $attendanceDetail->id]);
+
+                $isBreakInPenalty = false;
+            }
+
+            if (!$attendance->endBreakAttendanceDetail) {
+                $shiftTime = new Carbon($shift->break_ended_at);
+                $penalty = Penalty::where('attendance_type', AttendanceType::breakTime())
+                    ->where('company_id', $company->id)
+                    ->whereNull('lateness')
+                    ->first();
+                $attendanceDetail = AttendanceDetail::create([
+                    'attendance_type' => AttendanceType::breakEndedAt(),
+                    'attended_at' => null,
+                    'scheduled_at' => $today->copy()->addSeconds($shiftTime->secondsSinceMidnight()),
+                    'attendance_qr_code_id' => null,
+                    'image' => null,
+                    'ip' => '127.0.0.1',
+                    'location' => null,
+                    'verified_by' => null,
+                    'verified_at' => null,
+                ]);
+                if ($isBreakInPenalty) {
                     AttendancePenalty::create([
                         'penalty_amount' => $penalty->amount,
-                        'attendance_id' => $attendance->id,
+                        'attendance_detail_id' => $attendanceDetail->id,
                         'penalty_id' => $penalty->id,
                         'penalty_name' => $penalty->name,
-                        'note' => 'Tidak Scan Pulang'
+                        'note' => 'Tidak Scan Istirahat Masuk'
                     ]);
                 }
-
-                // no break
-                $breakOutAttendance = $todayAttendances->where('attendance_type', AttendanceType::breakStartedAt())
-                    ->where('shift_id', $employeeShift->shift_id)
-                    ->where('employee_id', $employee->id)
-                    ->whereDate('scheduled_at', $today)
-                    ->first();
-                if (!$breakOutAttendance) {
-                    $shiftTime = new Carbon($employeeShift->shift->break_started_at);
-                    $penalty = Penalty::where('attendance_type', AttendanceType::breakTime())
-                        ->where('company_id', $companyId)
-                        ->whereNull('lateness')
-                        ->first();
-                    $attendance = Attendance::create([
-                        'attendance_type' => AttendanceType::breakStartedAt(),
-                        'attended_at' => null,
-                        'scheduled_at' => $today->copy()->addSeconds($shiftTime->secondsSinceMidnight()),
-                        'attendance_qr_code_id' => null,
-                        'image' => null,
-                        'ip' => '127.0.0.1',
-                        'longitude' => null,
-                        'latitude' => null,
-                        'verified_by' => null,
-                        'verified_at' => null,
-                        'shift_id' => $employeeShift->shift_id,
-                        'employee_id' => $employee->id
-                    ]);
-                    AttendancePenalty::create([
-                        'penalty_amount' => $penalty->amount,
-                        'attendance_id' => $attendance->id,
-                        'penalty_id' => $penalty->id,
-                        'penalty_name' => $penalty->name,
-                        'note' => 'Tidak Scan Istirahat Keluar'
-                    ]);
-                    $isBreakInPenalty = false;
-                }
-
-                $breakInAttendance = $todayAttendances->where('attendance_type', AttendanceType::breakEndedAt())
-                    ->where('shift_id', $employeeShift->shift_id)
-                    ->where('employee_id', $employee->id)
-                    ->whereDate('scheduled_at', $today)
-                    ->first();
-                if (!$breakInAttendance) {
-                    $shiftTime = new Carbon($employeeShift->shift->break_ended_at);
-                    $penalty = Penalty::where('attendance_type', AttendanceType::breakTime())
-                        ->where('company_id', $companyId)
-                        ->whereNull('lateness')
-                        ->first();
-                    $attendance = Attendance::create([
-                        'attendance_type' => AttendanceType::breakEndedAt(),
-                        'attended_at' => null,
-                        'scheduled_at' => $today->copy()->addSeconds($shiftTime->secondsSinceMidnight()),
-                        'attendance_qr_code_id' => null,
-                        'image' => null,
-                        'ip' => '127.0.0.1',
-                        'longitude' => null,
-                        'latitude' => null,
-                        'verified_by' => null,
-                        'verified_at' => null,
-                        'shift_id' => $employeeShift->shift_id,
-                        'employee_id' => $employee->id
-                    ]);
-                    if ($isBreakInPenalty) {
-                        AttendancePenalty::create([
-                            'penalty_amount' => $penalty->amount,
-                            'attendance_id' => $attendance->id,
-                            'penalty_id' => $penalty->id,
-                            'penalty_name' => $penalty->name,
-                            'note' => 'Tidak Scan Istirahat Masuk'
-                        ]);
-                    }
-                }
+                $attendance->update(['end_break_id' => $attendanceDetail->id]);
             }
         }
-        return;
     }
 }
