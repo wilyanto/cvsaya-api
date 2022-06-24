0abb1ac990155c057b9f1fcdce0193b8a8b00e4d
William <Williamliang99@gmail.com>
2022-06-24 15:36:29 +0700

    change crm blast log column name

--- database/migrations/2022_06_23_135346_create_crm_blast_logs_table.php
+++ database/migrations/2022_06_23_135346_create_crm_blast_logs_table.php
@@ -15,18 +15,18 @@ return new class extends Migration
     {
         Schema::create('crm_blast_logs', function (Blueprint $table) {
             $table->id();
-            $table->string('model_type');
-            $table->integer('model_id');
-            $table->string('sender_country_code');
-            $table->string('sender_phone_number');
+            $table->string('blast_loggable_type');
+            $table->unsignedBigInteger('blast_loggable_id');
+            $table->unsignedBigInteger('credential_id');
             $table->string('recipient_country_code');
             $table->string('recipient_phone_number');
             $table->unsignedBigInteger('blast_type_id');
-            $table->json('message_variable');
+            $table->json('message_param_value');
             $table->json('message_template');
             $table->timestamps();
 
             $table->foreign('blast_type_id')->references('id')->on('blast_types');
+            $table->foreign('credential_id')->references('id')->on('crm_credentials');
         });
     }
 
