9be408a9b3ca90d3a79bacf27b67cb400bbc8e53
William <Williamliang99@gmail.com>
2022-06-28 10:01:43 +0700

    add credential search by name and phone number

---
+++ app/Http/Common/Filter/FilterCredentialSearch.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Common\Filter;
+
+use Spatie\QueryBuilder\Filters\Filter;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\DB;
+
+class FilterCredentialSearch implements Filter
+{
+    public function __invoke(Builder $query, $value, string $property): Builder
+    {
+        $query->where(function ($query) use ($value) {
+            $query = $query->where('name', 'LIKE', '%' . $value . '%')
+                ->orWhere(DB::raw('CONCAT(`country_code`, `phone_number`)'), 'LIKE', '%' . $value . '%');
+        });
+
+        return $query;
+    }
+}
