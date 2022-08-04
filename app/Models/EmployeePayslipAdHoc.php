9fc8e2dff2008778d16458c39390fcd4f7781235
kada <kada.indonesia@gmail.com>
2022-08-04 09:37:33 +0000

    adjust employee payslip adhoc

---
+++ app/Models/EmployeePayslipAdHoc.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EmployeePayslipAdHoc extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'company_salary_type_id',
+        'employee_payslip_id',
+        'name',
+        'date',
+        'amount',
+        'note'
+    ];
+
+    public function payslip()
+    {
+        return $this->belongsTo(Payslip::class);
+    }
+
+    public function companySalaryType()
+    {
+        return $this->belongsTo(CompanySalaryType::class);
+    }
+}
