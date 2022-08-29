b48f85ea121bbf1b1ea1cb3ca1c4b03c2b14a065
William <Williamliang99@gmail.com>
2022-08-29 16:16:04 +0700

    adjust leave permission and leave permission occasion

--- app/Http/Requests/LeavePermissionStoreRequest.php
+++ app/Http/Requests/LeavePermissionStoreRequest.php
@@ -24,7 +24,7 @@ class LeavePermissionStoreRequest extends FormRequest
     public function rules()
     {
         return [
-            'company_id' => 'required|exists:companies,id',
+            'employee_id' => 'required|exists:employees,id',
             'started_at' => 'required|date_format:Y-m-d H:i:s',
             'ended_at' => 'date_format:Y-m-d H:i:s|after:started_at',
             'occasion_id' => 'required|exists:leave_permission_occasions,id',
