f2e70eb7eedac9be27ce2200edf5340d2ef08700
William <Williamliang99@gmail.com>
2022-08-30 12:14:50 +0700

    add attendance penalty CRUD functionality

---
+++ app/Http/Common/Filter/FilterPenaltySearch.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Common\Filter;
+
+use Spatie\QueryBuilder\Filters\Filter;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\DB;
+
+class FilterPenaltySearch implements Filter
+{
+    public function __invoke(Builder $query, $value, string $property): Builder
+    {
+        return $query->where('name', 'LIKE', '%' . $value . '%');
+    }
+}
